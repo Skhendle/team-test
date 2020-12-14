b3e003692e88ef3230b64a2581ae14e984339a9c
Sandile Khendle <36414340+Skhendle@users.noreply.github.com>
2020-12-14 14:58:23 +0200

    Create test_functions.py
    Created test file, wrote comments and created skeleton functions for the tests.

---
+++ tests/test_functions.py
@@ -0,0 +1,17 @@
+from functions import AddNumbers, SubtractNumbers, DivideNumbers, MultiplyNumbers
+
+"""
+ test is the functions work as intented
+"""
+
+def test_addition():
+  pass
+
+def test_subtraction():
+  pass
+  
+def test_division():
+  pass
+  
+def test_multiplication():
+  pass
