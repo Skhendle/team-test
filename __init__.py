c16863a46c5c7c31966aea83954bfd851b95f0ad
Tumelo <tumie201511@gmail.com>
2020-12-15 12:27:37 +0200

    added a division class

--- __init__.py
+++ __init__.py
@@ -2,4 +2,6 @@ import os
 from functions import *
 
 if __name__ =="__main__":
-    print("The product of 6 and 13 is {}".format(MultiplyNumbers(6, 10).answer() ) )
+    
+    print("The product of 6 and 13 is {}".format(MultiplyNumbers(6, 10).answer() ) )    
+    print("The division of 1 and 2 is {}".format(DivideNumbers(1, 2).answer()) )
